292913b277a1bac2842d1cd0e9b711297da26efc
changwoo <ep6tri@hotmail.com>
2024-12-09 17:42:21 +0900

    update index.php base code

--- index.php
+++ index.php
@@ -9,3 +9,51 @@
  * Requires PHP:      8.3
  * Version:           0.1.0
  */
+
+if (!defined('ABSPATH')) {
+    return;
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+const ME_DIABETES_MAIN    = __FILE__;
+const ME_DIABETES_VERSION = '0.1.0';
+
+if (!function_exists('meDiabetes')) {
+    function meDiabetes(): \Bojaghi\Continy\Continy
+    {
+        static $instance = null;
+
+        if (is_null($instance)) {
+            try {
+                $instance = \Bojaghi\Continy\ContinyFactory::create(__DIR__ . '/conf/continy.php');
+            } catch (\Bojaghi\Continy\ContinyException|\Bojaghi\Continy\ContinyNotFoundException $e) {
+                wp_die($e->getMessage());
+            }
+        }
+
+        return $instance;
+    }
+}
+
+if (!function_exists('meDiabetesGet')) {
+    /**
+     * @template T
+     * @param class-string<T> $id
+     *
+     * @return T|object|null
+     */
+    function meDiabetesGet(string $id)
+    {
+        try {
+            $instance = meDiabetes()->get($id);
+        } catch (\Bojaghi\Continy\ContinyException|\Bojaghi\Continy\ContinyNotFoundException $e) {
+            $instance = null;
+        }
+
+        return $instance;
+    }
+}
+
+// Start our plugin.
+meDiabetes();
