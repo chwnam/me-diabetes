a678d92deb042b82e50196adb291c8108b425e84
changwoo <ep6tri@hotmail.com>
2024-12-09 17:01:00 +0900

    add README.md

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name:       Me Diabetes
+ * Plugin URI:        https://github.com/chwnam/me-diabetes
+ * Description:       A certain movement for improving your diabetes.
+ * Author:            changwoo
+ * Author URI:        https://blog.changwoo.pe.kr
+ * Requires at least: 5.2
+ * Requires PHP:      8.3
+ * Version:           0.1.0
+ */
